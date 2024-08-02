b02ab308bffcf0de2150b655f6a057a1674cfb70
svaderia <vaderiashyamal@gmail.com>
2024-08-02 12:08:22 -0400

    (script) Updated qutoes.js
    Now fetch the index from another file which generated such that we are
    cycling between the indices uniformly

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('https://raw.githubusercontent.com/svaderia/quote-of-the-day/main/quotes.json')
+  const today = new Date();
+  const month = today.getMonth() + 1; // getMonth() returns 0 (January) to 11 (December), so add 1
+  const day = today.getDate(); // get the current day (1-31)
+
+  // First fetch the dictionary for the current month
+  fetch(`https://raw.githubusercontent.com/svaderia/quote-of-the-day/main/quotes/${month}.json`)
     .then(response => response.json())
-    .then(quotes => {
-      const today = new Date().toISOString().split('T')[0];
-      const seed = today.split('-').join('');
-      const randomIndex = Math.floor(seededRandom(seed) * quotes.length);
-      const randomQuote = quotes[randomIndex];
-      document.getElementById('quote').innerText = randomQuote.quote;
-      document.getElementById('attribution').innerText = "- " + randomQuote.attribution;
+    .then(dayToIndex => {
+      // Use the day to get the index
+      const index = dayToIndex[day.toString()]; // Convert day to string to match the keys in the dictionary
+      console.log(index);
+      if (index === undefined) {
+        throw new Error(`No index found for day ${day}`);
+      }
+
+      // Now fetch the quotes array
+      return fetch('https://raw.githubusercontent.com/svaderia/quote-of-the-day/main/quotes.json')
+        .then(response => response.json())
+        .then(quotes => {
+          const randomQuote = quotes[index];
+          if (randomQuote) {
+            document.getElementById('quote').innerText = randomQuote.quote;
+            document.getElementById('attribution').innerText = "- " + randomQuote.attribution;
+          } else {
+            console.error('No quote found for the index', index);
+          }
+        });
     })
     .catch(error => console.error('Error fetching the quote:', error));
 });
-
-// https://stackoverflow.com/a/19303725
-function seededRandom(seed) {
-  let x = Math.sin(seed) * 10000;
-  return x - Math.floor(x);
-}
 
